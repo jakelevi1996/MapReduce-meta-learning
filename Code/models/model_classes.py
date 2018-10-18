759b422739a0aae25026a238d32da591f4b4e32a
jakelevi1996 <jakelevi@hotmail.co.uk>
2018-10-19 00:14:47 +0100

    Add meta-learning functionality (comments and doc-strings still to come)

--- Code/models/model_classes.py
+++ Code/models/model_classes.py
@@ -1,21 +1,25 @@
 import numpy as np
 import tensorflow as tf
 
-# TODO: 2 classes: Regressor, and TrainableRegressor which inherits from
-# Regressor? Because the meta-parameters won't be using any of tensorflow's
-# built-in training procedures, they'll all be manual based on meta-learning
-# algorithms
 
 # TODO: add tensorboard summaries, especially for gradients
 
-class Regressor():
-    
+
+class ReptileRegressor():
     def __init__(
         self,
-        input_dim=1, num_hidden_units=15,
+        # Network-params:
+        input_dim=1, num_layers=2, num_hidden_units=15,
         hidden_activation=tf.tanh,
-        learning_rate=0.01,
-        ):
+        # Meta-params:
+        inner_learning_rate=0.01, outer_learning_rate=1,
+        num_inner_steps=5, num_outer_steps=5,
+    ):
+        self.inner_learning_rate = inner_learning_rate
+        # self.outer_learning_rate = outer_learning_rate|there is no outer rate
+        self.num_inner_steps = num_inner_steps
+        self.num_outer_steps = num_outer_steps
+
         # Define network
         # TODO: add regulariser
         # TODO: option for multiple layers
@@ -46,96 +50,112 @@ class Regressor():
             labels=self.y_placeholder,
             predictions=self.y_op
         )
-        self.optimizer = tf.train.AdamOptimizer(learning_rate)
+        self.optimizer = tf.train.AdamOptimizer(inner_learning_rate)
         self.train_op = self.optimizer.minimize(self.loss_op)
 
-        # Create op for initialising variables
-        self.init_op = tf.global_variables_initializer()
-
         # Create summaries, for visualising in Tensorboard
-        # Training summaries:
-        # tf.summary.scalar("Accuracy", accuracy)
-        # variables = tf.trainable_variables()
-        # gradients = tf.gradients(self.loss_op, variables)
-        # self.train_summary_op = tf.summary.merge([
-        #     tf.summary.scalar("Train_loss", self.loss_op),
-        #     *[tf.summary.histogram(parse_name(v.name), v) for v in variables],
-        #     *[tf.summary.histogram(parse_name(g.name), g) for g in gradients]
-        # ])
-        # # Test summaries:
-        # self.test_summary_op = tf.summary.merge([
-        #     tf.summary.scalar("Test_loss", self.loss_op),
-        #     # <Other test summaries>
-        # ])
+        # ...
         
+        # ***
+        # NB the following statements all depend on calls to
+        # tf.global_variables(), so any tf.Variables added to the graph after
+        # them will not be taken into account (NB tf.Operations and
+        # tf.placeholders are not instances of tf.Variables):
+        # ***
+        # oh look it's an init op
+        self.init_op = tf.initializers.global_variables()
+        # (does this want to be defined here, or within a method?):
+        self.uninitialised_op = tf.report_uninitialized_variables()
+        # ... <descriptive comment>
+        self.global_vars = tf.global_variables()
+        # ... etc
+        self.parameter_placeholder_list = [
+            tf.placeholder(dtype=tf.float32) for _ in self.global_vars
+        ]
+        # var.assign actually returns a Tensor, not an Operation...
+        # see https://www.tensorflow.org/api_docs/python/tf/Variable#assign
+        # ... but I like to think of assignment as an operation
+        self.parameter_assign_op_list = [
+            var.assign(placeholder) for (var, placeholder) in zip(
+                self.global_vars, self.parameter_placeholder_list
+            )
+        ]
+        # ...
+        # NB axis must be set to zero because of the list-comprehension...
+        self.reduced_mean_tensor_list = [
+            tf.reduce_mean(
+                input_tensor=placeholder,
+                axis=0
+            ) for placeholder in self.parameter_placeholder_list
+        ]
+        # Can insert an operation here in between reduce_mean and assign, EG
+        # Bayesian inference for parameters by using previous value as a prior
+        self.assign_reduced_mean_op_list = [
+            var.assign(reduced_mean) for (var, reduced_mean) in zip(
+                self.global_vars, self.reduced_mean_tensor_list
+            )
+        ]
 
-    def initialize_variables(self, sess):
+    def initialise_variables(self, sess):
         sess.run(self.init_op)
 
     def predict(self, sess, x):
         predictions = sess.run(
-            self.y_op,
+            fetches=self.y_op,
             feed_dict={self.x_placeholder: x}
         )
         return predictions
+    
+    def eval_loss(self, sess, x, y):
+        sess.run(
+            fetches=self.loss_op,
+            feed_dict={
+                self.x_placeholder: x,
+                self.y_placeholder: y
+            }
+        )
 
     def training_step(self, sess, x, y):
         sess.run(
-            self.train_op,
+            fetches=self.train_op,
             feed_dict={
                 self.x_placeholder: x,
                 self.y_placeholder: y
             }
         )
     
-    # def training_step_with_summary(self, sess, x_train, y_train):
-    #     train_loss_val, train_summary_val, _ = sess.run([
-    #         self.loss_op,
-    #         self.train_summary_op,
-    #         self.train_op],
-    #         feed_dict={
-    #             self.input_placeholder: x_train,
-    #             self.labels_placeholder: y_train
-    #         }
-    #     )
-    #     return train_loss_val, train_summary_val
+    def get_global_vars(self, sess):
+        return sess.run(self.global_vars)
     
-    # def test_set_summary(self, sess, x_test, y_test):
-    #     test_loss_val, test_summary_val = sess.run([
-    #         self.loss_op,
-    #         self.test_summary_op],
-    #         feed_dict={
-    #             self.input_placeholder: x_test,
-    #             self.labels_placeholder: y_test
-    #         }
-    #     )
-    #     return test_loss_val, test_summary_val
-
-class ReptileRegressor():
-    def __init__(
-        self,
-        input_dim=1,
-        inner_learning_rate=0.001, outer_learning_rate=1,
-        num_layers=2, num_units=15, hidden_activation=tf.tanh,
-    ):
-        self.inner_learning_rate = inner_learning_rate
-        self.outer_learning_rate = outer_learning_rate
-
-        # TODO: Make sure vairable_scope is correct and should be used instead
-        # of name_scope
-        with tf.variable_scope("Meta_parameters"):
-            self.meta_network = Regressor()
-        with tf.variable_scope("Task_specific_parameters"):
-            self.task_specific_network = Regressor()
+    def set_global_vars(self, sess, global_vars):
+        # `global_vars` must come from a call to the `get_global_vars` method;
+        # could maybe use an `assert` statement for the shape of each param?
+        sess.run(
+            fetches=self.parameter_assign_op_list,
+            feed_dict={
+                placeholder: value for placeholder, value in zip(
+                    self.parameter_placeholder_list, global_vars
+                )
+            }
+        )
     
-    def inner_update(
-        self,
-
-    ):
-        pass
+    def adapt(self, sess, x, y):
+        # In reality this will be some steps of gradient descent
+        for _ in range(self.num_inner_steps):
+            self.training_step(sess, x, y)
     
-    def outer_update(
-        self,
-        
-    ):
-        pass
+    def meta_update(self, sess, params_list):
+        sess.run(
+            fetches=self.assign_reduced_mean_op_list,
+            feed_dict={
+                placeholder: [
+                    params[val_index] for params in params_list
+                ] for (placeholder, val_index) in zip(
+                    self.parameter_placeholder_list,
+                    range(len(self.parameter_placeholder_list))
+                )
+            }
+        )
+
+class Maml():
+    pass
